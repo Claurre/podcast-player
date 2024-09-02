8337e50c6556a82261ca43cb84444f6244799db2
Claurre <yurumaru@gmail.com>
2024-09-02 17:50:06 -0300

    tp2

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,13 @@ import Header from './Componentes/Header.tsx'
 import ListenAgain from './Componentes/ListenAgain.tsx'
 import SimilarArtist from './Componentes/SimilarArtist.tsx'
 import RecomendenAlbum from './Componentes/RecomendedAlbums.tsx'
+import AppTP2 from './Componentes/TP2/AppTP2.tsx'
+import Sidebar from './Componentes/TP2/Sidebar.tsx'
+import Formulario from './Componentes/TP2/Formulario.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Header/>
-    <ListenAgain/>
-    <SimilarArtist/>
-    <RecomendenAlbum/>
+    <AppTP2/>
   </React.StrictMode>,
 )
